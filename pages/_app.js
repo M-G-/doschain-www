f601e4b82de0174709cd044dbfba11257042edf6
MG <mgding@dotdotbuy.com>
2019-10-14 21:00:18 +0800

    update

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,15 @@ import App from 'next/app'
 import { appWithTranslation } from '../i18n'
 
 class MyApp extends App {
-  // static async getInitialProps ({ Component, router, ctx }) {
-  //   let pageProps = {}
-  //
-  //   if (Component.getInitialProps) {
-  //     pageProps = await Component.getInitialProps(ctx)
-  //   }
-  //
-  //   return {pageProps}
-  // }
+  static async getInitialProps ({ Component, router, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return {pageProps}
+  }
 
   render() {
     const { Component, pageProps } = this.props
