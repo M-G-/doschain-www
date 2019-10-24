17fcc1b655501d819b53d2c78319d154cd08fba3
MG <mgding@dotdotbuy.com>
2019-10-24 19:19:09 +0800

    update: BTC价格预警接口联调

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link'
 import BasicLayout from '../components/BasicLayout'
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Form, Button, InputNumber, Input, Icon, Switch, Table } from 'antd';
+import { Form, Button, InputNumber, Input, Icon, Switch, Spin, Table, Modal, Tooltip } from 'antd';
 import { i18n, withTranslation } from '../i18n'
 import axios from '../utils/axios'
 
@@ -10,64 +10,84 @@ function hasErrors(fieldsError) {
   return Object.keys(fieldsError).some(field => fieldsError[field]);
 }
 
+const apiRoot = 'http://api.m.doschain.org/monitoring';
+
 class Homepage extends React.Component{
 
   static async getInitialProps () {
-    const {data} = await axios.get('https://blockchain.info/ticker')
-
     return {
       namespacesRequired: ['common'],
-      aData: data
     }
   }
 
   componentDidMount() {
-    // To disabled submit button at the beginning.
-    this.props.form.validateFields();
+    this.getBtcPrice();
+    this.getList();
   }
 
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
+  getBtcPrice = () =>{
+    this.setState({aData: null})
+
+    // axios.get('https://blockchain.info/ticker').then(({data}) =>{
+    axios.get(`${location.origin}/huobi_api/market/history/trade?symbol=btcusdt&size=1`).then(({data: _data}) =>{
+      // const {data}
+      const {status, data} = _data
+      console.log(data)
+      if(status === 'ok'){
+        this.setState({currentPrice: data[0].data[0].price})
       }
-    });
-  };
+
+    }, (e) => {
+      console.log('e: ', e)
+    })
+  }
+
+  getList = () => {
+    axios.get(`${apiRoot}/warn/get-price`).then(({data: _data}) =>{
+      console.log(_data)
+      const {data, errno, errmsg} = _data
+
+      if(errno === 0) {
+        this.setState({dataSource: (()=>{
+          return (data.price || []).map(item => ({
+            key: `${parseInt(`${Math.random() * 1e10}`)}`,
+            amount: item,
+            status: 0
+          }))
+          })()})
+      }
+    })
+  }
 
   static propTypes = {
     t: PropTypes.func.isRequired,
   }
 
   state = {
-    number: 1000,
+    currentPrice: null,
     editKey: -1,
-    dataSource: [
-      {
-        key: '1',
-        amount: 5000,
-        status: 1
-      },
-      {
-        key: '2',
-        amount: 6000,
-        status: 0
-      },
-      {
-        key: '3',
-        amount: 7000,
-        status: 0
-      },
-    ]
+    loading: false,
+    dataSource: []
   }
 
-  onChange = (value) => {
-    console.log('changed', value);
+  addItem = () => {
+    this.getList()
+  }
+
+  deleteItem = (amount) => {
+    axios.get(`${apiRoot}/warn/del-price?price=${amount}`).then(({data: _data}) =>{
+      console.log(_data)
+      const {data, errno, errmsg} = _data
+
+      if(errno === 0) {
+        this.getList()
+      }
+    })
   }
 
   render(){
-    const {aData, t} = this.props
-    const {dataSource, editKey} = this.state
+    const {t} = this.props
+    const {dataSource, currentPrice} = this.state
 
     // const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
 
@@ -79,36 +99,15 @@ class Homepage extends React.Component{
         title: '金额（$）',
         dataIndex: 'amount',
         key: 'amount',
-        render: (amount, {key}) => (
-          key === editKey
-            ? <div><InputNumber
-              min={0}
-              onChange={this.onChange}
-              onBlur={()=>{
-                this.setState({editKey: -1})
-              }}
-              defaultValue={amount}
-              autoFocus
-              style={{width: 180}} /></div>
-            : <div onClick={() => {
-              this.setState({editKey: key})
-            }} style={{cursor: 'pointer'}} title='修改'>{amount}</div>
-        ),
-      },
-      {
-        title: '状态',
-        dataIndex: 'status',
-        key: 'status',
-        render: (status) => (
-          <Switch checked={status === 1} />
-        ),
       },
       {
         title: '操作',
         dataIndex: 'action',
         key: 'action',
-        render: (status) => (
-          <Button type='danger' size='small'>删除</Button>
+        render: (status, {amount}) => (
+          <Button type='danger' size='small' onClick={()=> {
+            this.deleteItem(amount)
+          }}>删除</Button>
         ),
       },
     ];
@@ -116,8 +115,19 @@ class Homepage extends React.Component{
     return(
       <BasicLayout title={t('h1')}>
 
-        <p style={{marginTop: 20}}>BTC价格: ${aData.USD.last} / ¥{aData.CNY.last}</p>
-        <p style={{marginTop: 15}} />
+        <div style={{marginTop: 20, fontSize: 24, height: 50}}>
+          BTC价格:&nbsp;
+          {currentPrice === null
+            ? <Spin style={{marginLeft: 10}} />
+            : (
+              <Tooltip placement='bottom' title='点击刷新'>
+                  <span style={{cursor: 'pointer'}} onClick={this.getBtcPrice}>
+                    ${currentPrice}
+                  </span>
+              </Tooltip>
+            )
+          }
+        </div>
         <Table
           dataSource={dataSource}
           columns={columns}
@@ -125,41 +135,142 @@ class Homepage extends React.Component{
         />
         <p style={{marginTop: 15}} />
         <Button type='primary' onClick={()=>{
-          this.setState(({dataSource})=>{
-            const key = `${parseInt(Math.random() * 1e10)}`
-            dataSource.push({
-              key,
-              amount: '',
-              status: 0
-            },)
-
-            return {
-              editKey: key,
-              dataSource
-            }
-          })
+          // this.setState(({dataSource})=>{
+          //   const key = `${parseInt(Math.random() * 1e10)}`
+          //   dataSource.push({
+          //     key,
+          //     amount: '',
+          //     status: 0
+          //   },)
+          //
+          //   return {
+          //     editKey: key,
+          //     dataSource
+          //   }
+          // })
+          this.addModal.showModal()
 
         }}><Icon type='plus' /> 添加</Button>
-        {/*<Form layout="inline" onSubmit={this.handleSubmit}>
-          <Form.Item validateStatus={numberError ? 'error' : ''} help={numberError || ''}>
-            {getFieldDecorator('number', {
-              rules: [{ required: true, message: 'Please input BTC amount!' }],
-            })(
-              <InputNumber
-                min={0}
-                onChange={this.onChange}
-                formatter={value => `$ ${value}`}
-                style={{width: 180}} />
-            )}
-          </Form.Item>
-
-          <Form.Item>
-            <Switch defaultChecked onChange={this.onChange} />
-          </Form.Item>
-        </Form>*/}
+        <AddModalWidthForm
+          wrappedComponentRef={(ref) => {this.addModal = ref}}
+          addItem={this.addItem}
+        />
       </BasicLayout>
     );
   }
 }
 
-export default withTranslation('index')(Form.create({ name: 'horizontal_login' })(Homepage))
+class AddModal extends React.Component {
+  componentDidMount() {
+    this.props.form.validateFields();
+  }
+
+  state = {
+    visible: false,
+    confirmLoading: false,
+    number: 0
+  };
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    },() => {
+      this.input && this.input.focus()
+    });
+  };
+
+  handleOk = () => {
+    const { form } = this.props;
+    form.validateFields((err, {number}) => {
+      if (err) {
+        return;
+      }
+
+      const value = parseInt(number);
+      if(isNaN(value) || value <=0) {
+        console.log('请输入大于0的数字')
+        this.props.form.setFields({
+          number: {
+            errors: [new Error('请输入大于0的数字')],
+          },
+        });
+        return;
+      }
+
+      this.setState({
+        confirmLoading: true,
+      });
+      // setTimeout(() => {
+      //
+      // }, 2000);
+      axios.get(`${apiRoot}/warn/price?price=${value}`).then(({data: _data}) =>{
+        console.log(_data)
+        const {data, errno, errmsg} = _data
+
+        if(errno === 0) {
+          this.setState({
+            visible: false,
+            confirmLoading: false,
+          });
+          this.props.addItem(value);
+          form.resetFields();
+        }else{
+          // this.setState({
+          //   visible: false,
+          //   confirmLoading: false,
+          // });
+        }
+        // this.setState({aData: data})
+      })
+    });
+
+  };
+
+  handleCancel = () => {
+    this.setState({
+      visible: false,
+    });
+  };
+
+  render() {
+    const { visible, confirmLoading} = this.state;
+
+    const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
+
+    // Only show error after a field is touched.
+    const numberError = isFieldTouched('number') && getFieldError('number');
+
+    return (
+      <Modal
+        title='添加预警价格'
+        visible={visible}
+        onOk={this.handleOk}
+        confirmLoading={confirmLoading}
+        onCancel={this.handleCancel}
+        okText='添加'
+        cancelText='取消'
+        okButtonProps={{disabled: hasErrors(getFieldsError())}}
+      >
+        <Form layout='vertical' hideRequiredMark>
+          <Form.Item
+            label='预警价格（$）'
+            validateStatus={numberError ? 'error' : ''}
+            help={numberError || ''}
+          >
+            {getFieldDecorator('number', {
+              rules: [
+                { required: true, message: '请输入预警价格' },
+                { pattern: /^\d*\.?\d*$/, message: '请输入正确的价格' },
+              ],
+            })(
+              <Input ref={(ref)=>{this.input = ref}} autoFocus/>
+            )}
+          </Form.Item>
+        </Form>
+      </Modal>
+    );
+  }
+}
+
+const AddModalWidthForm = Form.create({ name: 'add_item' })(AddModal)
+export default withTranslation('index')(Homepage)
--- server.js
+++ server.js
@@ -5,12 +5,30 @@ const nextI18NextMiddleware = require('next-i18next/middleware').default
 const nextI18next = require('./i18n')
 
 const port = process.env.PORT || 80
-const app = next({ dev: process.env.NODE_ENV !== 'production' })
+const env = process.env.NODE_ENV
+const dev = env !== 'production'
+const app = next({ dev: dev})
 const handle = app.getRequestHandler();
+
+const devProxy = {
+  '/huobi_api': {
+    target: 'https://api.huobi.pro/',
+    pathRewrite: { '^/huobi_api': '/' },
+    changeOrigin: true
+  }
+};
+
 
 (async () => {
   await app.prepare()
   const server = express()
+
+  if (devProxy) {
+    const proxyMiddleware = require('http-proxy-middleware')
+    Object.keys(devProxy).forEach(function (context) {
+      server.use(proxyMiddleware(context, devProxy[context]))
+    })
+  }
 
   server.use(nextI18NextMiddleware(nextI18next))
 
